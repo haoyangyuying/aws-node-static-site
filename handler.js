03420ab75fe4ca6bb2f32206f5b4030ea236e1fc
Howard Yang <howard@shoplineapp.com>
2020-04-18 17:52:22 +0800

    feat(AddTemplates): Add templates from webslides

--- handler.js
+++ handler.js
@@ -1,5 +1,4 @@
 const serverless = require("serverless-http");
-const hbs = require("hbs");
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -7,7 +6,6 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.set("view engine", "hbs");
 
 app.get("/", function(req, res) {
   res.status(200).render("index");
