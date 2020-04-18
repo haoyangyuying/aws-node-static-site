65271ed8c5433474481e6baaa6d3bb2dc2984980
Howard Yang <howard@shoplineapp.com>
2020-04-18 18:01:03 +0800

    fix: update page

--- handler.js
+++ handler.js
@@ -1,4 +1,5 @@
 const serverless = require("serverless-http");
+const hbs = require("hbs");
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -6,6 +7,8 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.set('view engine', 'hbs');
+
 
 app.get("/", function(req, res) {
   res.status(200).render("index");
